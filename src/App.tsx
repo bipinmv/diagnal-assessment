600ca9042cae4076e9d8390d2e14c1f1f29cc829
Bipin M V <bipin.mv@prevalent.ai>
2024-11-14 01:14:54 +0530

    Updated style

--- src/App.tsx
+++ src/App.tsx
@@ -5,104 +5,104 @@ import Search from "./components/Search";
 import { API_BASE_URL } from "./config/constants";
 
 interface Item {
-  name: string;
-  "poster-image": string;
+	name: string;
+	"poster-image": string;
 }
 
 const App: React.FC = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [page, setPage] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [stopFetching, setStopFetching] = useState(false);
-  const [isSearching, setSearching] = useState(false);
-  const observerTarget = useRef(null);
+	const [items, setItems] = useState<Item[]>([]);
+	const [searchQuery, setSearchQuery] = useState<string>("");
+	const [page, setPage] = useState<number>(1);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [stopFetching, setStopFetching] = useState(false);
+	const [isSearching, setIsSearching] = useState(false);
+	const observerTarget = useRef(null);
 
-  const fetchData = async (pageNum: number) => {
-    if (loading) return;
-    setLoading(true);
-    try {
-      const response = await fetch(
-        `${API_BASE_URL}/data/page${pageNum}.json`
-      ).then((data) => data.json());
-      const data = (await response?.page?.["content-items"]?.content) ?? [];
-      setItems((prevItems) => [...prevItems, ...data]);
-    } catch (error) {
-      console.error("Error fetching data", error);
-      setStopFetching(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+	const fetchData = async (pageNum: number) => {
+		if (loading) return;
+		setLoading(true);
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/data/page${pageNum}.json`
+			).then(data => data.json());
+			const data = (await response?.page?.["content-items"]?.content) ?? [];
+			setItems(prevItems => [...prevItems, ...data]);
+		} catch (error) {
+			console.error("Error fetching data", error);
+			setStopFetching(true);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const loadMoreContent = useCallback(async () => {
-    await fetchData(page);
-    setPage((prev) => prev + 1);
-  }, [page]);
+	const loadMoreContent = useCallback(async () => {
+		await fetchData(page);
+		setPage(prev => prev + 1);
+	}, [page]);
 
-  useEffect(() => {
-    loadMoreContent();
-  }, []);
+	useEffect(() => {
+		loadMoreContent();
+	}, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && !loading) {
-          loadMoreContent();
-        }
-      },
-      { threshold: 1.0 }
-    );
-    if (observerTarget.current) {
-      observer.observe(observerTarget.current);
-    }
-    return () => observer.disconnect();
-  }, [loadMoreContent, loading]);
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			entries => {
+				if (entries[0].isIntersecting && !loading) {
+					loadMoreContent();
+				}
+			},
+			{ threshold: 1.0 }
+		);
+		if (observerTarget.current) {
+			observer.observe(observerTarget.current);
+		}
+		return () => observer.disconnect();
+	}, [loadMoreContent, loading]);
 
-  const filteredItems = items.filter((item) =>
-    item.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+	const filteredItems = items.filter(item =>
+		item.name.toLowerCase().includes(searchQuery.toLowerCase())
+	);
 
-  return (
-    <main>
-      <div className="sticky-nav">
-        {!isSearching ? (
-          <div className="list-nav">
-            <img
-              src={`${API_BASE_URL}/images/Back.png`}
-              alt="search"
-              width="18"
-              className="cursor-pointer"
-            />
-            <span>Romantic Comedy</span>
-            <img
-              src={`${API_BASE_URL}/images/search.png`}
-              alt="search"
-              width="20"
-              onClick={() => setSearching(true)}
-              className="search-icon"
-            />
-          </div>
-        ) : (
-          <Search
-            searchQuery={searchQuery}
-            onSearchChange={setSearchQuery}
-            setSearching={setSearching}
-          />
-        )}
-      </div>
-      <div className="container">
-        <Grid items={filteredItems} />
-        {!searchQuery && !stopFetching && (
-          <div ref={observerTarget} style={{ height: "4px" }} />
-        )}
-        {loading && (
-          <div className="mt-4 text-center text-white">
-            Loading more content...
-          </div>
-        )}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="sticky-nav">
+				{!isSearching ? (
+					<div className="list-nav">
+						<img
+							src={`${API_BASE_URL}/images/Back.png`}
+							alt="search"
+							width="18"
+							className="cursor-pointer"
+						/>
+						<span>Romantic Comedy</span>
+						<img
+							src={`${API_BASE_URL}/images/search.png`}
+							alt="search"
+							width="20"
+							onClick={() => setIsSearching(true)}
+							className="search-icon"
+						/>
+					</div>
+				) : (
+					<Search
+						searchQuery={searchQuery}
+						onSearchChange={setSearchQuery}
+						setSearching={setIsSearching}
+					/>
+				)}
+			</div>
+			<div className="container">
+				<Grid items={filteredItems} />
+				{!searchQuery && !stopFetching && (
+					<div ref={observerTarget} style={{ height: "4px" }} />
+				)}
+				{loading && (
+					<div className="mt-4 text-center text-white">
+						Loading more content...
+					</div>
+				)}
+			</div>
+		</main>
+	);
 };
 export default App;
